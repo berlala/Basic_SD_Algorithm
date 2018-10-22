7301ad645a7d1f0b026729181e940195a65a6f5c
Bolin Zhao(FF China) <Bolin.zhao@faradayfuture.com>
2018-10-22 16:08:59 +0800

    update quintic

--- Quintic_Planner/quintic_planner.m
+++ Quintic_Planner/quintic_planner.m
@@ -1,7 +1,7 @@
 %% Quintic Plan Plan 
 % Bolin Zhao , FF China, bolin.zhao@ff.com
 %%
-function [xt_log] = quintic_planner(xs, vxs,axs,xe,vxe,axe,T,t) 
+function [xt_log, vt_log, at_log, jt_log] = quintic_planner(xs, vxs,axs,xe,vxe,axe,T,t) 
 
 A = [T^3, T^4, T^5;
          3*T^2, 4*T^3, 5*T^4;
@@ -21,8 +21,22 @@ a4 = x(2);
 a5 = x(3);
 
 xt_log = [];
+vt_log = [];
+at_log =[];
+jt_log =[];
+
 for ts = 0:t:T
     xt = a0 + a1* ts + a2*ts^2 + a3*ts^3 + a4*ts^4 + a5*ts^5;
-   xt_log = [xt_log, xt];
+    xt_log = [xt_log, xt];
+    
+    vt = a1+ 2*a2*ts +3*a3*ts^2 + 4*a4*ts^3 + 5*a5*ts^4;
+    vt_log = [vt_log, vt];
+    
+    at =  2*a2 + 6*a3*ts + 12*a4*ts^2 + 20*a5*ts^3;
+    at_log = [at_log, at];
+
+    jt =  6*a3 + 24*a4*ts + 60*a5*ts^2;
+    jt_log = [jt_log, jt];
+
 end
 return
--- Quintic_Planner/test_quintic.m
+++ Quintic_Planner/test_quintic.m
@@ -1,34 +1,80 @@
 % Test Quintic Planner
 %%
 % Bolin Zhao, ff china, bolin.zhao@ff.com
-%%
-clc;clear;
-%%
-T =  5;
+%% Config
+clc;clear;close all
+V_Threshold =3;
+Acc_Threshold = 2; % m/s^2
+Jerk_Threshold = 0.5; % m/s^3
+T_Max =  100; % Max Allow Time 
 t = 0.1;
-
+%% States
 % Start State
-xs = 0;
-vxs = 0;
-axs = 0.1;
-ys = 0;
-vys = 1;
+xs = 10;
+ys = 10;
+vxs =1;
+vys = 0;
+axs =0.1;
 ays = 0;
 
 
 % End State
 xg = 50;
+yg = 20;
 vxg = 1;
+vyg = 0;
 axg =0;
-yg = 10;
-vyg = 1;
 ayg = 0;
 
-% Path Geneartion
+%% Path Geneartion
 
-[xt] = quintic_planner(xs, vxs,axs,xg,vxg,axg,T,t) ;
-[yt] = quintic_planner(ys, vys,ays,yg,vyg,ayg,T,t) ;
+for T =1:1:T_Max
+Result_true = 0;
+[xt,vxt,axt,jxt] = quintic_planner(xs, vxs,axs,xg,vxg,axg,T,t) ;
+[yt,vyt,ayt,jyt] = quintic_planner(ys, vys,ays,yg,vyg,ayg,T,t) ;
 
+for i = 1:length(xt)
+    if sqrt(axt(i)^2+ayt(i)^2)>Acc_Threshold|| sqrt(jxt(i)^2+jxt(i)^2)>Jerk_Threshold || sqrt(vxt(i)^2+vyt(i)^2)>V_Threshold
+        Result_true = 1 ;
+    else
+        Result_true = Result_true;
+    end
+end
+if Result_true ==0
+    break
+end
+
+end
 %% Result
-plot(xt,yt)
-title('')
+plot(xt,yt);hold on
+title('Local Path Map')
+quiver(xs,ys,vxs,vys,'linewidth',2)
+quiver(xg,yg,vxg,vyg,'linewidth',2)
+xlabel('X')
+ylabel('Y')
+
+v = [sqrt(vxs^2+vys^2)];
+a = [sqrt(axs^2+ays^2)];
+jerk =[0];
+for ii = 2:length(vxt)
+    v(ii) = sqrt(vxt(ii)^2+vyt(ii)^2);
+    if v(ii) - v(ii-1)<0 % deceleration
+        dec = -1;
+    else
+        dec = 1;
+    end
+    a(ii) = sqrt(axt(ii)^2+ayt(ii)^2)*dec;
+    if a(ii) - a(ii-1)<0 % deceleration
+        dec_a = -1;
+    else
+        dec_a = 1;
+    end
+    jerk(ii) = sqrt(jxt(ii)^2+jxt(ii)^2)*dec_a;
+end
+
+time = (0:1:length(v)-1)*t;
+figure(2)
+plot(time, v);hold on
+plot(time,a)
+plot(time,jerk)
+legend('Velocity[m/s]','Acceleration','Jerk')
