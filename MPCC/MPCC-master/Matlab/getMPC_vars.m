d916170e6e27716e6f72b70763017bc558e1bd78
Bolin <bolinzhao@yahoo.com>
2018-12-24 18:34:01 +0800

    success version for MPCC

--- MPCC/MPCC-master/Matlab/getMPC_vars.m
+++ MPCC/MPCC-master/Matlab/getMPC_vars.m
@@ -21,7 +21,7 @@ if CarModel == 'ORCA'
     % MPC settings %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % prediction horizon
-    MPC_vars.N = 10;%original 40
+    MPC_vars.N = 20;%original 40
     % sampling time
     MPC_vars.Ts = 0.02;
     % used model (TODO incorparate new models)
@@ -32,34 +32,34 @@ if CarModel == 'ORCA'
     % state-input scaling %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % normalization matricies (scale states and inputs to ~ +/- 1
-    MPC_vars.Tx = diag(1./[3,3,2*pi,4,2,7,30]);
-    MPC_vars.Tu = diag(1./[1,0.35,6]);
-
-    MPC_vars.invTx = diag([3,3,2*pi,4,2,7,30]);
-    MPC_vars.invTu = diag([1,0.35,6]);
-
+%     MPC_vars.Tx = diag(1./[3,3,2*pi,4,2,7,30]);
+%     MPC_vars.Tu = diag(1./[1,0.35,6]);
+% 
+%     MPC_vars.invTx = diag([3,3,2*pi,4,2,7,30]);
+%     MPC_vars.invTu = diag([1,0.35,6]);
+% 
+%     MPC_vars.TDu = eye(3);
+%     MPC_vars.invTDu = eye(3);
+    % identity matricies if inputs should not be normalized
+    MPC_vars.Tx = eye(7);
+    MPC_vars.Tu = eye(3);
+    
+    MPC_vars.invTx = eye(7);
+    MPC_vars.invTu = eye(3);
+    
     MPC_vars.TDu = eye(3);
     MPC_vars.invTDu = eye(3);
-    % identity matricies if inputs should not be normalized
-    % MPC_vars.Tx = eye(7);
-    % MPC_vars.Tu = eye(3);
-    % 
-    % MPC_vars.invTx = eye(7);
-    % MPC_vars.invTu = eye(3);
-    % 
-    % MPC_vars.TDu = eye(3);
-    % MPC_vars.invTDu = eye(3);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % state-input bounds %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     % bounds for non-nomalized state-inputs
-    % MPC_vars.bounds = [-3,-3,-10,-0.1,-2,-7,   0,  -0.1,-0.35,0  ,  -1 ,-1,-5;
-    %                     3, 3, 10,   4, 2, 7,  30,     1, 0.35,5  ,   1 , 1, 5]'; 
+    MPC_vars.bounds = [-3,-3,-10,-0.1,-2,-7,   0,  -0.1,-0.35,0  ,  -1 ,-1,-5;
+                                              3, 3, 10,   4, 2, 7,  30,     1, 0.35,5  ,   1 , 1, 5]'; 
     % bounds for nomalized state-inputs (bounds can be changed by changing
     % % normalization)
-    MPC_vars.bounds = [-1,-1,-3, 0,-1,-1,   0,  -0.1,-1,0  ,  -1 ,-1,-5;
-                        1, 1, 3, 1, 1, 1,   1,     1, 1,1  ,   1 , 1, 5]'; 
+%     MPC_vars.bounds = [-1,-1,-3, 0,-1,-1,   0,  -0.1,-1,0  ,  -1 ,-1,-5;
+%                                               1, 1, 3, 1, 1, 1,   1,     1, 1,1  ,   1 , 1, 5]'; 
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Cost Parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -73,7 +73,7 @@ if CarModel == 'ORCA'
 
     MPC_vars.rD= 1e-4; % cost on duty cycle (only used as regularization terms)
     MPC_vars.rDelta= 1e-4; % cost on steering 
-    MPC_vars.rVtheta= 1e-4*100; % cost on virtual velocity
+    MPC_vars.rVtheta= 1e-4; % cost on virtual velocity
 
     MPC_vars.rdD= 0.01; % cost on change of duty cycle
     MPC_vars.rdDelta= 1; % cost on change of steering
