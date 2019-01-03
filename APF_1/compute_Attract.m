cbbe1bd6123497d34db739f0ed4a3c470add8c28
Bolin <bolinzhao@yahoo.com>
2019-01-03 16:55:20 +0800

    remove APF 1

--- APF_1/compute_Attract.m
+++
@@ -1,7 +0,0 @@
-function [Yatx,Yaty]=compute_Attract(X,Xsum,k,angle,b,Po,n)%输入参数为当前坐标，目标坐标，增益常数,分量和力的角度
-%把路径上的临时点作为每个时刻的Xgoal
-R=(X(1)-Xsum(1,1))^2+(X(2)-Xsum(1,2))^2;%路径点和目标的距离平方
-r=sqrt(R);%路径点和目标的距离
-Yatx=k*r*cos(angle);%angle=Y(1)
-Yaty=k*r*sin(angle);
-end
